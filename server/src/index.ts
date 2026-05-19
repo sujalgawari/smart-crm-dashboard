fd354e4f84cd9a795be5af901b006c107dec7099
Sujal <sujalgaware64@gmail.com>
2026-05-19 17:02:58 +0530

    Added production CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,28 +10,47 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://smart-crm-dashboard-zeta.vercel.app",
+    ],
     credentials: true,
   })
 );
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/leads", leadRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(process.env.MONGO_URI as string)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use("/api/auth", authRoutes);
+
+app.use("/api/leads", leadRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+
+    console.log(
+      "MongoDB Connected"
+    );
+
+    app.listen(
+      process.env.PORT || 5000,
+      () => {
+
+        console.log(
+          "Server running on port 5000"
+        );
+
+      }
+    );
+
+  })
+  .catch((err) =>
+    console.log(err)
+  );
